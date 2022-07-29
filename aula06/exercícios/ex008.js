bd5a8a2558c2218922ca783f2a947e422f1bdd7f
Thiago Souza <throberto85@gmail.com>
2022-07-29 10:47:50 -0400

    Arquivos

---
+++ aula06/exercícios/ex008.js
@@ -0,0 +1,7 @@
+var vel = 61 /*condição simples*/
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+      console.log('Você ultrapassou a velocidade permitida, multado!!')
+}
+
+console.log(`Dirija sempre com cinto de segurança.`)
---
+++ aula06/exercícios/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo no país ${país}`)
+if (país != 'Brasil') {
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é estrngeiro')
+}
